eaec5c08f72e2907cf55be7f83341154dbb239a3
bysoyeon <167726398+bysoyeon@users.noreply.github.com>
2024-08-17 23:24:07 +0900

    fix: 청소년 유해업소, 음식 판매여부 내용 반영

--- src/main/java/com/example/jejuddai/repository/StoreRepository.java
+++ src/main/java/com/example/jejuddai/repository/StoreRepository.java
@@ -3,7 +3,7 @@ package com.example.jejuddai.repository;
 import com.example.jejuddai.entity.Store;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;ㅎ
+import java.util.List;
 
 public interface StoreRepository extends JpaRepository<Store, String> {
 //    List<Store> findByCategoryAndLatitudeBetweenAndLongitudeBetween(String category, double latStart, double latEnd, double lonStart, double lonEnd);
@@ -13,8 +13,8 @@ public interface StoreRepository extends JpaRepository<Store, String> {
 
 
     // 카테고리 제거
-    List<Store> findByLatitudeBetweenAndLongitudeBetween(double latStart, double latEnd, double lonStart, double lonEnd);
+//    List<Store> findByLatitudeBetweenAndLongitudeBetween(double latStart, double latEnd, double lonStart, double lonEnd);
 
     // isNotGoodForChild가 false이고, isFoodSelling이 true인 Store 엔티티 리스트를 반환
-    List<Store> findByIsNotGoodForChildFalseAndIsFoodSellingTrue();
+//    List<Store> findByIsNotGoodForChildFalseAndIsFoodSellingTrue();
 }
